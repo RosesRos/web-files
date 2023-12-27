69d330f10327d1a7ed7d022574a7766a25b86cf3
RosesRos <rostranespinoza@gmail.com>
2023-12-27 12:55:09 +0300

    update

---
+++ shablonV2/UK/js/index.js
@@ -0,0 +1,121 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+
+  function timer1() {
+    const timerEls = document.querySelectorAll('.stringWithTime');
+    const timerTextEls = [];
+    timerEls.forEach(element => {
+      timerTextEls.push(element.querySelector('span[class="timer"]'));
+    });
+    const hours = 0;
+    const minutes = 46;
+    const seconds = 50;
+    let countdownDate = new Date();
+    countdownDate.setHours(countdownDate.getHours() + hours);
+    countdownDate.setMinutes(countdownDate.getMinutes() + minutes);
+    countdownDate.setSeconds(countdownDate.getSeconds() + seconds);
+    timerTextEls.forEach(element => {
+      const x = setInterval(function() {
+        const now = new Date().getTime();
+        const distance = countdownDate - now;
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        element.textContent = `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+        if (distance < 0) {
+          clearInterval(x);
+          timerTextEl.textContent = '00:00:00';
+        }
+      }, 1000);
+    });
+  };
+  timer1();
+
+  function accordion() {
+    const btn = document.querySelectorAll('.accordion');
+
+    btn.forEach((item, index) => {
+      item.addEventListener('click', function () {
+        let currentParent = this.closest('.accordion');
+        currentParent.classList.toggle('active');
+
+        let currentArrow = document.querySelectorAll('.accordion-arrow');
+        for (let ind = 0; ind < currentArrow.length; ind++) {
+          if (index == ind) {
+            currentArrow[ind].classList.toggle("active");
+          }
+        }
+      });
+    });
+
+  };
+  accordion();
+
+
+  function accordionFaqs() {
+    const btn = document.querySelectorAll('.accordion-faq');
+
+    btn.forEach((item, index) => {
+      item.addEventListener('click', function () {
+        let currentParent = this.closest('.accordion-faq');
+        currentParent.classList.toggle('active');
+
+        let closeArrow = document.querySelectorAll('.close-arrow');
+        for (let ind = 0; ind < closeArrow.length; ind++) {
+          if (index == ind) {
+            closeArrow[ind].classList.toggle("active");
+          }
+        }
+      });
+    });
+
+  };
+  accordionFaqs();
+
+
+  function LoaderBox() {
+    var loadMoreContent = document.querySelector('div[data-id="3158:436"]');
+    if (loadMoreContent) {
+      loadMoreContent.addEventListener("click", function (e) {
+        var loadMoreEl = document.querySelector('div[data-id="3158:436"]');
+        var progressEl = document.querySelector("#progress");
+    
+        loadMoreEl.style.display = "none";
+        progressEl.style.display = "flex";
+      });
+    }
+  }
+  LoaderBox();
+
+  let moreText = document.querySelector('div[data-id="I3210:465;3107:9437;3107:9431"]');
+  function handleDropdownFooter() {
+    let e = document.querySelector('.hidden-text'),
+    o = document.querySelector('div[data-id="I3210:465;3107:9437;3107:9431"]');
+    e.classList.contains('show') ? (o.textContent = 'Read more') : (o.textContent = 'Read less'),
+    e.classList.toggle('show');
+  }
+  moreText.addEventListener("click", function() {
+    handleDropdownFooter();
+  });
+
+  function SliderBox() {
+    new Swiper(".swiper", {
+      loop: true,
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      }
+    });
+  }
+  SliderBox();
+});
