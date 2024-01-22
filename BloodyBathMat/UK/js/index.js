0f44e941dcf6c6a258f7cdc885461c16bc86484d
RosesRos <rostranespinoza@gmail.com>
2024-01-22 23:35:46 +0300

    update

---
+++ BloodyBathMat/UK/js/index.js
@@ -0,0 +1,191 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  function timer1() {
+    // const timerEls = document.querySelectorAll('.stringWithTime');
+    // const timerTextEls = [];
+    // timerEls.forEach(element => {
+    //   timerTextEls.push(element.querySelector('span[class="timer"]'));
+    // });
+
+    const hour = document.querySelector(".hour");
+    const minu = document.querySelector(".minu");
+    const seco = document.querySelector(".seco");
+
+    const hours = 0;
+    const minutes = 46;
+    const seconds = 50;
+    let countdownDate = new Date();
+    countdownDate.setHours(countdownDate.getHours() + hours);
+    countdownDate.setMinutes(countdownDate.getMinutes() + minutes);
+    countdownDate.setSeconds(countdownDate.getSeconds() + seconds);
+
+    const tim = setInterval(function() {
+      const now = new Date().getTime();
+      const distance = countdownDate - now;
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      hour.textContent = `${hours < 10 ? '0' + hours : hours}`;
+      seco.textContent = `${seconds < 10 ? '0' + seconds : seconds}`;
+      minu.textContent = `${minutes < 10 ? '0' + minutes : minutes}`;
+      if (distance < 0) {
+        clearInterval(tim);
+        hour.textContent = "02";
+        minu.textContent = "46";
+        seco.textContent = '50';
+      }
+    }, 1000);
+
+    // timerTextEls.forEach(element => {
+    //   const x = setInterval(function() {
+    //     const now = new Date().getTime();
+    //     const distance = countdownDate - now;
+    //     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    //     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    //     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    //     element.textContent = `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+    //     if (distance < 0) {
+    //       clearInterval(x);
+    //       timerTextEl.textContent = '00:00:00';
+    //     }
+    //   }, 1000);
+    // });
+  };
+  timer1();
+
+
+  function accordion() {
+    const btn = document.querySelectorAll('.accordion');
+
+    btn.forEach((item, index) => {
+      item.addEventListener('click', function () {
+        let currentParent = this.closest('.accordion');
+        currentParent.classList.toggle('active');
+
+        let currentArrow = document.querySelectorAll('.accordion-arrow');
+        for (let ind = 0; ind < currentArrow.length; ind++) {
+          if (index == ind) {
+            currentArrow[ind].classList.toggle("active");
+          }
+        }
+      });
+    });
+
+  };
+  accordion();
+
+
+  function accordionFaqs() {
+    const btn = document.querySelectorAll('.accordion-faq');
+
+    btn.forEach((item, index) => {
+      item.addEventListener('click', function () {
+        let currentParent = this.closest('.accordion-faq');
+        currentParent.classList.toggle('active');
+
+        let closeArrow = document.querySelectorAll('.close-arrow');
+        for (let ind = 0; ind < closeArrow.length; ind++) {
+          if (index == ind) {
+            closeArrow[ind].classList.toggle("active");
+          }
+        }
+      });
+    });
+
+  };
+  accordionFaqs();
+
+
+  function LoaderBox() {
+    var loadMoreContent = document.querySelector('div[data-id="1206:5506"]');
+    if (loadMoreContent) {
+      loadMoreContent.addEventListener("click", function (e) {
+        var loadMoreEl = document.querySelector('div[data-id="1206:5506"]');
+        var progressEl = document.querySelector("#progress");
+    
+        loadMoreEl.style.display = "none";
+        progressEl.style.display = "flex";
+      });
+    }
+  }
+  LoaderBox();
+
+  let moreText = document.querySelector('div[data-id="I1206:6452;1206:6391;546:2175"]');
+  function handleDropdownFooter() {
+    let e = document.querySelector('.hidden-text'),
+    o = document.querySelector('div[data-id="I1206:6452;1206:6391;546:2175"]');
+    e.classList.contains('show') ? (o.textContent = 'Read more') : (o.textContent = 'Read less'),
+    e.classList.toggle('show');
+  }
+  moreText.addEventListener("click", function() {
+    handleDropdownFooter();
+  });
+
+  function scrollBox() {
+    const button1 = document.getElementById('button1');
+    const button2 = document.getElementById('button2');
+    const scroElement = document.getElementById('scro');
+    const scrollThreshold = scroElement.offsetTop; // Порог прокрутки до элемента scro
+
+    window.addEventListener('scroll', function () {
+      // Получаем текущую позицию прокрутки
+      const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+
+      // Проверяем, достигли ли порога
+        if (scrollPosition >= scrollThreshold) {
+          // Показываем кнопку 2 и скрываем кнопку 1
+          button2.style.bottom = "10px"
+        } else {
+          // Скрываем кнопку 2 и показываем кнопку 1
+          button2.style.bottom = "-10rem"
+        }
+    });
+  }
+  scrollBox();
+
+  function SliderBox3() {
+    new Swiper(".mySwiper3", {
+      spaceBetween: 30,
+      loop: true,
+      slidesPerView: "auto",
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination-3",
+        clickable: true,
+      },
+    });
+  }
+  SliderBox3();
+
+
+  function SliderBox4() {
+    new Swiper(".mySwiper4", {
+      spaceBetween: 30,
+      slidesPerView: "auto",
+      centeredSlides: false,
+      initialSlide: 0,
+    });
+  }
+  SliderBox4();
+
+
+  function boxPage() {
+    const items = document.querySelectorAll(".c-box-imgs");
+
+    items[1].classList.add("active");
+    items.forEach((item, index) => {
+      item.addEventListener("click", function() {
+        items.forEach((ite) => {
+          ite.classList.remove("active");
+        });
+        item.classList.toggle("active");
+      });
+    }); 
+  }
+  boxPage();
+
+});
