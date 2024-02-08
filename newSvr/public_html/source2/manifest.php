debf42aa7d58c0bec7ca3edafdcf14619fffa9ac
RosesRos <rostranespinoza@gmail.com>
2024-02-08 15:58:41 +0300

    update

---
+++ newSvr/public_html/source2/manifest.php
@@ -0,0 +1,45 @@
+﻿{
+    "dir":"ltr",
+    "name":"777 Casino",
+    "scope":"/",
+    "display":"standalone",
+    "start_url":"https://crazytimes.club/source/offline.html",
+    "short_name":"777 Casino",
+    "theme_color":"#ffffff",
+    "description":"777 Casino",
+    "orientation":"any",
+    "background_color":"#ffffff",
+    "prefer_related_applications":false,
+    "icons":[
+        {
+            "src":"https://crazytimes.club/source/img/icon.png",
+            "sizes":"512x512",
+            "type":"image/png",
+            "purpose":"any"
+        }
+    ],
+    "url":"https://crazytimes.club/",
+    "related_applications":[{
+        "platform": "webapp",
+        "url": "https://crazytimes.club/source/manifest.php"
+    }],
+    "screenshots":[
+
+    ],
+    "generated":"",
+    "manifest_package":"org.chromium.webapk.test",
+    "scope_url":"https://crazytimes.club/",
+    "intent_filters":{
+        "scope_url_scheme":"https",
+        "scope_url_host":"crazytimes.club,
+        "scope_url_path":"/"
+    },
+    "display_mode":"standalone",
+    "web_manifest_url":"https://crazytimes.club/source/manifest.php",
+    "version_code":"1",
+    "version_name":"1.0",
+    "bound_webapk":{
+        "runtime_host":"org.chromium.chrome",
+        "runtime_host_application_name":"Chromium"
+    }
+}
